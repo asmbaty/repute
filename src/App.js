befa0438e6888486ceb7ac5137f6a300d5bda71d
Aharon Smbatyan <aharon.smbatyan@gmail.com>
2021-05-15 21:01:12 +0400

    Connect to web3 and receive user account address

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Web3 from 'web3';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends Component {
+  
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if(window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable() // browser
+    }
+    else if(window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider) // metamask
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: ''
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Welcome to my React + Truffle applicatoin
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          User account address <b>{this.state.account}</b>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
