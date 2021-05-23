0cd5102c8e56ddcf8b04c7e2122f6d9ef91ce985
Aharon Smbatyan <aharon.smbatyan@gmail.com>
2021-05-23 18:43:20 +0400

    Fix compiler warnings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,15 @@
 import React, {Component} from 'react';
 import { AppBar, Toolbar, Grid, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 
 class Header extends Component {
-    constructor(props) {
-        super(props)
-    }    
 
     render() {
         return (
         <AppBar position="static">
         <Toolbar>
             <Grid container justify="space-between">  
-                <Typography inline variant="h6" align="left">{this.props.title}</Typography>
-                <Typography inline align="right">
+                <Typography variant="h6" align="left">{this.props.title}</Typography>
+                <Typography align="right">
                     User: {this.props.address}
                 </Typography>
             </Grid>
